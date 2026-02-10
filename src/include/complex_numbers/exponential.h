d7fb684df28fd62981ca126adab10333ccbe1f1c
Дамир <abdurahmonovdamir60@gmail.com>
2026-02-10 10:30:30 +0500

    updated comments for doxygen

--- src/include/complex_numbers/exponential.h
+++ src/include/complex_numbers/exponential.h
@@ -1,13 +1,15 @@
 #ifndef COMPLEXNUMBERS_EXPONENTIAL_H
 #define COMPLEXNUMBERS_EXPONENTIAL_H
 
+#include "complex_numbers/complex.h"
+
 namespace ComplexNumbers {
     /**
      * @brief Экспоненциальная форма комплексного числа.
      *
      * Заглушка для будущей реализации.
      */
-    class Exponential {
+    class Exponential : public Complex {
     };
 } // ComplexNumbers
 
